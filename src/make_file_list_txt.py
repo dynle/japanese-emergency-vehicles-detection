3457ee140f6abe49a2377594cfe7099f12f69f8e
dynle <dynleedev@gmail.com>
2022-12-22 21:33:30 +0900

    Update directory path

--- src/make_file_list_txt.py
+++ src/make_file_list_txt.py
@@ -1,12 +1,12 @@
 import os
 
-dir = "./dataset/project-5-at-2022-12-21-08-38-9ce678bc/images/train"
+dir = "./dataset/images/train"
 
 root, dirs, files = next(os.walk(dir, topdown=True))
 files = [ os.path.join(root, f) for f in files ]
 # print(files)
 
 
-with open('./dataset/project-5-at-2022-12-21-08-38-9ce678bc/train.txt', 'w') as f:
+with open('./dataset/train.txt', 'w') as f:
     for line in files:
         f.write(f"{line}\n")
